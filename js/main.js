76da237c4b9cbab5e0e6b957a5ee70e04123d3fb
Nail Sharipov <sharipovn@gmail.com>
2022-06-30 23:05:42 +0500

    https://threejs.org/manual/#en/lights and https://threejs.org/manual/#en/shadows

--- js/main.js
+++ js/main.js
@@ -19,7 +19,11 @@ function setupRenderer() {
     const renderer = new THREE.WebGLRenderer({canvas});
     // see also: https://usefulangle.com/post/12/javascript-going-fullscreen-is-rare
     // renderer.setPixelRatio( window.devicePixelRatio ); // This is strongly NOT RECOMMENDED
-    renderer.setSize( canvas.clientWidth, canvas.clientHeight, false ); // -1 helps to avoid scrollbars in Chrome
+    renderer.setSize( canvas.clientWidth, canvas.clientHeight, false );
+    // see https://threejs.org/manual/#en/shadows
+    renderer.shadowMap.enabled = true;
+    console.log("Max Texture Size: " + renderer.capabilities.maxTextureSize);
+
     return renderer;
 }
 
@@ -50,8 +54,35 @@ function setupDirLight() {
     dirLight.position.y = 2;
     dirLight.position.z = 0;
     dirLight.target.position.set(0, 0, 0);
-    dirLight.intensity = 3;
+    dirLight.intensity = 2;
+
+    // Shadows (see https://threejs.org/manual/#en/shadows)
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.position.x = 2;
+    dirLight.shadow.camera.position.y = 2;
+    dirLight.shadow.camera.position.z = 0;
+    dirLight.shadow.camera.lookAt(0, 0, 0);
+
+    dirLight.shadow.camera.top = 4;
+    dirLight.shadow.camera.bottom = -4;
+    dirLight.shadow.camera.left = -4;
+    dirLight.shadow.camera.right = 4;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 20;
+
+    // to avoid low-resolution shadows 
+    dirLight.shadow.mapSize.width = 2048;
+    dirLight.shadow.mapSize.height = 2048;
+
     return dirLight;
+}
+
+function setupAmbLight() {
+    // see https://threejs.org/manual/#en/lights
+    const color = 0xFFFFFF;
+    const intensity = 0.1;
+    const light = new THREE.AmbientLight(color, intensity);
+    return light;
 }
 
 function setupOrbitControls(camera, renderer) {
@@ -71,26 +102,68 @@ function makeCube() {
     cube.position.x = -1;
     cube.position.y = 0;
     cube.position.z = 0;
-    
-    console.log(cube);
+
+    // see https://threejs.org/manual/#en/shadows
+    cube.castShadow = true;
+    cube.receiveShadow = true;
 
     return cube;
+}
+
+function makeGround() {
+    const planeSize = 20;
+    // see https://threejs.org/manual/#en/lights
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load('assets/images/checker.png');
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2;
+    texture.repeat.set(repeats, repeats);
+
+    // see https://threejs.org/manual/#en/lights
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({
+        map: texture,
+        side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh.rotation.x = Math.PI * -.5;
+
+    // see https://threejs.org/manual/#en/shadows
+    mesh.receiveShadow = true;
+
+    return mesh;
 }
 
 const renderer = setupRenderer();
 const camera = setupPerspectiveCamera();
 const scene = setupScene(); 
-
-const dirLight = setupDirLight();
-scene.add( dirLight );
-scene.add( dirLight.target );
-
 const orbitControls = setupOrbitControls(camera, renderer);
 
+// LIGHTS
+const dirLight = setupDirLight();
+scene.add( dirLight );
+const ambLight = setupAmbLight();
+scene.add(ambLight);
+
+// HELPERS
+// see https://threejs.org/manual/#en/lights
+const dirLightHelper = new THREE.DirectionalLightHelper(dirLight);
+scene.add(dirLightHelper);
+// see https://threejs.org/manual/#en/shadows
+const cameraHelper = new THREE.CameraHelper(dirLight.shadow.camera);
+scene.add(cameraHelper);
+
+// OBJECTS
 const cube = makeCube();
+console.log(cube);
 scene.add(cube);
 
-let apple;
+const ground = makeGround();
+scene.add(ground);
+
+let apple = new THREE.Object3D();
 // see https://threejs.org/docs/index.html#manual/en/introduction/Loading-3D-models
 const loader = new GLTFLoader();
 loader.load( 'assets/3d/foodKit_v1.2/Models/GLTF/apple.glb', function ( gltf ) {
@@ -98,7 +171,15 @@ loader.load( 'assets/3d/foodKit_v1.2/Models/GLTF/apple.glb', function ( gltf ) {
     console.log(gltf);
 
     if (gltf && gltf.scene && gltf.scene instanceof THREE.Object3D) {
-        apple = gltf.scene;
+        apple = Object.assign(gltf.scene);
+        apple.traverse(function(object) {
+            if (object.isMesh) {
+                // see https://threejs.org/manual/#en/shadows
+                object.castShadow = true;
+                object.receiveShadow = true;
+            }
+        })
+
         scene.add( gltf.scene );
     }
 
